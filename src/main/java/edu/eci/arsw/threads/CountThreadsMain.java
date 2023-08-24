958fa6409278e7b4331206e0f5bb90f1818df4f8
MpulidoM <marianapulido075@gmail.com>
2023-08-24 10:32:12 -0500

    Punto2 de la PARTE 1.

--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -15,9 +15,9 @@ public class CountThreadsMain {
         CountThread Thread1 = new CountThread(0,99);
         CountThread Thread2 = new CountThread(99,199);
         CountThread Thread3 = new CountThread(200,299);
-        Thread1.start();
-        Thread2.start();
-        Thread3.start();
+        Thread1.run();
+        Thread2.run();
+        Thread3.run();
     }
     
 }
